e83c6431f1f0931cf2f7852ac9e71d616011c62f
mathesh <matheshyogeswaran@gmail.com>
2023-02-24 21:22:18 +0530

    add extra details project

--- routes/project.js
+++ routes/project.js
@@ -12,32 +12,38 @@ projectRoute.route("/projects/getProjectDetails").get(function (req, res) {
   });
 });
 
-projectRoute.route("/projects/getIncompleteProjectDetails").get(function (req, res) {
-  Project.find({completeStatus:false}, (err, projects) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(projects);
-    }
+projectRoute
+  .route("/projects/getIncompleteProjectDetails")
+  .get(function (req, res) {
+    Project.find({ completeStatus: false }, (err, projects) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(projects);
+      }
+    });
   });
-});
 
 projectRoute.route("/projects/addBasicProjDetails").post(function (req, res) {
   try {
     // variable should be in the name as in the model
-    const projectName = req.body.projectName;
-    const description = req.body.description;
-    const technology = req.body.technology;
+    const clientDetails = req.body.clientDetails;
+    const gitHubLink = req.body.gitHubLink;
+    const jiraLink = req.body.jiraLink;
     const projectDeadLine = req.body.deadline;
-    const techLead = req.body.techlead;
+    const contributors = req.body.contributors;
+    const initiatedOn = Date.now();
+    const projectManager = "4";
 
     // we are creating a new object of the model
     const project = new Project({
-      projectName,
-      description,
-      technology,
+      clientDetails,
+      gitHubLink,
+      jiraLink,
       projectDeadLine,
-      techLead,
+      contributors,
+      initiatedOn,
+      projectManager,
     });
 
     // we are saving the data to the database
@@ -64,5 +70,45 @@ projectRoute.route("/projects/addBasicProjDetails").post(function (req, res) {
   }
 });
 
+projectRoute.route("/projects/addExtraProjDetails").post(function (req, res) {
+  try {
+    // variable should be in the name as in the model
+    const gitHubLink = req.body.gitHubLink;
+    const jiraLink = req.body.jiraLink;
+    const clientDetails = {
+      clientName:req.body.clientName,
+      clientAddress:req.body.clientAddress,
+      clientPhoneNumber:req.body.clientPhone
+    }
+    // we are creating a new object of the model
+    const project = new Project({
+      clientDetails,
+      gitHubLink,
+      jiraLink,
+    });
+
+    // we are saving the data to the database
+    project
+      .save()
+      .then((item) =>
+        res.json({
+          message: "Project Extra Details added successfully",
+          status: true,
+        })
+      )
+      .catch((err) => {
+        // error code 11000 is for duplicate data in mongo db
+        if (err.code === 11000) {
+          return res.json({
+            message: "Project already exists",
+            status: false,
+          });
+        }
+        res.status(500).send({ error: "Error saving data to the database" });
+      });
+  } catch {
+    return res.json([{ message: "Data Not Found", status: "false" }]);
+  }
+});
 
 module.exports = projectRoute;
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,35 @@ const userRoute = express.Router();
 const User = require("../models/user.model");
 
 userRoute.route("/users/getTechLead").get(function (req, res) {
-  User.find({ useRoleName: "Techlead" },(err, projects) => {
+  User.find({ useRoleName: "Techlead" }, (err, projects) => {
+    if (err) {
+      res.send(err);
+    } else {
+      // const usersArray = projects.map((item) => item.users);
+      // res.json(usersArray);
+      res.json(projects[0].users);
+    }
+  });
+});
+
+userRoute.route("/users/getContributors").get(function (req, res) {
+  User.find(
+    { useRoleName: { $in: ["Developer", "Techlead", "BA"] } },
+    (err, projects) => {
       if (err) {
         res.send(err);
       } else {
-        // const usersArray = projects.map((item) => item.users);
-        // res.json(usersArray);
-        res.json(projects[0].users);
+
+        var data = projects.map((item) => {
+          var eachUserData = item.users.map((userData) => {
+            userData["userRoleName"] = item.useRoleName;  // add userRoleName to each user
+            return userData;
+          });
+          return eachUserData;
+        });
+        res.json([].concat(...data));
+
+
       }
     }
   );
