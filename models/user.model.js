60313bcdebd1dd6791f73e67146c8527f6d95020
Kajanth9 <skajanthranbir@gmail.com>
2023-03-23 19:46:03 +0530

    login

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const User = new mongoose.Schema(
   {
-    useRoleName: { type: String },
+    userRoleName: { type: String },
     fname: { type: String },
     lname: { type: String },
     email: { type: String, required:true, unique:true },
     phone: { type: String },
     password: { type: String},
     userStatus: { type: Boolean},
-    orangechangeHrLink: { type: String },
-    userGitHubLink: { type: String },
+    orangeHrLink: { type: String },
+    GitHubUsername: { type: String },
     userJiraLink: { type: String },
     userImage: [{ type: Object}],
     feedback: { type: String}
