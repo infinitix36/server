bda94e57e276bff44f287276f69953820f37c0c7
Mathesh <99591069+matheshyogeswaran@users.noreply.github.com>
2023-03-31 19:42:36 +0530

    profile pic github

--- routes/user.js
+++ routes/user.js
@@ -1,48 +1,52 @@
 const express = require("express");
 const userRoute = express.Router();
 const User = require("../models/user.model");
-const fetch = require('node-fetch');
-
+const fetch = require("node-fetch");
 
 userRoute.route("/users/all").get(function (req, res) {
   const { userRoleName, sortBy, sortOrder } = req.query;
   const query = userRoleName ? { userRoleName } : {};
   let sortCriteria = {};
-  if (sortBy === 'fname') {
-    sortCriteria.fname = sortOrder === 'asc' ? 1 : -1;
-  } else if (sortBy === 'rating') {
-    sortCriteria.rating = sortOrder === 'asc' ? 1 : -1;
+  if (sortBy === "fname") {
+    sortCriteria.fname = sortOrder === "asc" ? 1 : -1;
+  } else if (sortBy === "rating") {
+    sortCriteria.rating = sortOrder === "asc" ? 1 : -1;
   }
-  User.find(query, { _id: 1, fname: 1, rating: 1 , GitHubUsername: 1, userRoleName:1 })
-      .sort(sortCriteria)
-      .exec((err, users) => {
-        if (err) {
-          res.send(err);
-        } else {
-          const parsedUsers = users.map(user => ({
-            _id: user._id,
-            fname: user.fname,
-            rating: parseInt(user.rating),
-            userRoleName: user.userRoleName,
-            GitHubUsername: user.GitHubUsername
-          }));
-          res.json(parsedUsers);
-        }
-      });
+  User.find(query, {
+    _id: 1,
+    fname: 1,
+    rating: 1,
+    GitHubUsername: 1,
+    userRoleName: 1,
+  })
+    .sort(sortCriteria)
+    .exec((err, users) => {
+      if (err) {
+        res.send(err);
+      } else {
+        const parsedUsers = users.map((user) => ({
+          _id: user._id,
+          fname: user.fname,
+          rating: parseInt(user.rating),
+          userRoleName: user.userRoleName,
+          GitHubUsername: user.GitHubUsername,
+        }));
+        res.json(parsedUsers);
+      }
+    });
 });
 
 //orange hr leave fetch
-userRoute.route("/users/leave/:id").get(function(req, res) {
-  User.find({_id: req.params.id},{taken:1},(err,users)=>{
-    if(err) {
+userRoute.route("/users/leave/:id").get(function (req, res) {
+  User.find({ _id: req.params.id }, { taken: 1 }, (err, users) => {
+    if (err) {
       res.send(err);
-    }else {
+    } else {
       res.json(users);
     }
-
-  })
-})
-//verify user 
+  });
+});
+//verify user
 userRoute.route("/users/verifyuser").post(async (req, res) => {
   const result = req.body.result;
   const userID = req.body.userid;
@@ -90,18 +94,22 @@ userRoute.route("/users/usersToApproved").get(function (req, res) {
 
 // get rate of users ratings
 userRoute.route("/users/getRate").get(function (req, res) {
-  User.find({}, { _id: 1, fname: 1, rating: 1 ,GitHubUsername:1 ,}, (err, users) => {
-    if (err) {
-      res.send(err);
-    } else {
-      const parsedUsers = users.map(user => ({
-        _id: user._id,
-        fname: user.fname,
-        rating: parseInt(user.rating)
-      }));
-      res.json(parsedUsers);
+  User.find(
+    {},
+    { _id: 1, fname: 1, rating: 1, GitHubUsername: 1 },
+    (err, users) => {
+      if (err) {
+        res.send(err);
+      } else {
+        const parsedUsers = users.map((user) => ({
+          _id: user._id,
+          fname: user.fname,
+          rating: parseInt(user.rating),
+        }));
+        res.json(parsedUsers);
+      }
     }
-  });
+  );
 });
 
 // add ratings for user
@@ -381,17 +389,16 @@ userRoute.route("/users/getDeveloper/alphabet").get(function (req, res) {
 //   ).sort(sortQuery);
 // });
 
-
-
-userRoute.route('/users/getLogin').get(async function(req, res) {
+userRoute.route("/users/getLogin").get(async function (req, res) {
   const { GitHubUsername } = req.query;
 
   // Make request to GitHub API to retrieve number of commits for user
-  const response = await fetch(`https://api.github.com/users/${GitHubUsername}/events`);
+  const response = await fetch(
+    `https://api.github.com/users/${GitHubUsername}/events`
+  );
   const events = await response.json();
-  const commits = events
-    .filter(event => event.type === 'PushEvent')
-    //.reduce((acc, event) => acc + event.payload.commits.length, 0);
+  const commits = events.filter((event) => event.type === "PushEvent");
+  //.reduce((acc, event) => acc + event.payload.commits.length, 0);
 
   res.json({ commits });
 });
